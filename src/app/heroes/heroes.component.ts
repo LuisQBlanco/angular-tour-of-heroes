c319988c403f2b83c0977706aac38eca9150b8fa
Luis Q <ingluisq@gmail.com>
2018-11-06 12:39:19 -0500

    Multi pages dashboard, list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './../hero';
-import { HEROES } from './../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +8,21 @@ import { HEROES } from './../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
 
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    /*this.heroes = this.heroService.getHeroes();*/
+
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
